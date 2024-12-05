d15ed505715ff1f3d1d77192cfddb0976822a26a
unknown <tiagojavier29@gmail.com>
2024-12-05 01:16:23 -0500

    proyecto final UTP

--- src/main/java/com/soporte/digital/util/ConstantesApp.java
+++ src/main/java/com/soporte/digital/util/ConstantesApp.java
@@ -5,7 +5,7 @@ public class ConstantesApp {
     public static final int ROL_ADMIN = 1;
     public static final int ROL_CLIENTE = 6;
 
-    public static final String CORREO_ENVIA_REMIT = "recuperarUTP@gmail.com";
-    public static final String PASSWORD_ENVIA_REMIT = "yllhxwcjdnsaqplx";
+    public static final String CORREO_ENVIA_REMIT = "sop.rtedigital1@gmail.com";
+    public static final String PASSWORD_ENVIA_REMIT = "pjprmigeeblynjfv";
 
 }
